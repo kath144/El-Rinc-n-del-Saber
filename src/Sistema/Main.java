92daaee922203f6eeeb322303150861f16cb0017
kath144 <mscatalino2@gmail.com>
2025-07-07 18:07:22 -0500

    Agregada búsqueda por título con árbol binario

--- src/Sistema/Main.java
+++ src/Sistema/Main.java
@@ -4,7 +4,6 @@ import Vista.VentanaPrincipal;
 
 public class Main {
     public static void main(String[] args) {
-        // Para que la interfaz gráfica se ejecute correctamente
         javax.swing.SwingUtilities.invokeLater(() -> {
             new VentanaPrincipal().setVisible(true);
         });

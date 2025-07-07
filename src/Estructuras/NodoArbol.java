b51709aeb779cb8afd3bed74f63929d4849fb1f2
kath144 <mscatalino2@gmail.com>
2025-07-07 18:07:06 -0500

    Agregada búsqueda por título con árbol binario

--- src/Estructuras/NodoArbol.java
+++ src/Estructuras/NodoArbol.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Estructuras;
 
-/**
- *
- * @author hp
- */
+import Modelo.Libro;
+
 public class NodoArbol {
-    
+    private Libro libro;
+    NodoArbol izquierdo;
+    NodoArbol derecho;
+
+    public NodoArbol(Libro libro) {
+        this.libro = libro;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
 }

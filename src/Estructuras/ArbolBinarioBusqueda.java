d848366be40f44baefa3454567e9d2d00d07627d
kath144 <mscatalino2@gmail.com>
2025-07-07 18:06:59 -0500

    Agregada búsqueda por título con árbol binario

--- src/Estructuras/ArbolBinarioBusqueda.java
+++ src/Estructuras/ArbolBinarioBusqueda.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Estructuras;
 
-/**
- *
- * @author hp
- */
+import Modelo.Libro;
+
+
 public class ArbolBinarioBusqueda {
-    
+    private NodoArbol raiz;
+
+    public void insertar(Libro libro) {
+        raiz = insertarRecursivo(raiz, libro);
+    }
+
+    private NodoArbol insertarRecursivo(NodoArbol actual, Libro libro) {
+        // ordena por título
+        if (actual == null) return new NodoArbol(libro);
+
+        if (libro.getTitulo().compareToIgnoreCase(actual.getLibro().getTitulo()) < 0)
+            actual.izquierdo = insertarRecursivo(actual.izquierdo, libro);
+        else
+            actual.derecho = insertarRecursivo(actual.derecho, libro);
+
+        return actual;
+    }
+
+    public Libro buscar(String titulo) {
+        return buscarRecursivo(raiz, titulo);
+    }
+
+    private Libro buscarRecursivo(NodoArbol actual, String titulo) {
+        if (actual == null) return null;
+
+        int comparacion = titulo.compareToIgnoreCase(actual.getLibro().getTitulo());
+
+        if (comparacion == 0)
+            return actual.getLibro();
+        else if (comparacion < 0)
+            return buscarRecursivo(actual.izquierdo, titulo);
+        else
+            return buscarRecursivo(actual.derecho, titulo);
+    }
 }
